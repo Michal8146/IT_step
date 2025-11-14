f1f8f7090a4280958838637488dd6ea5a188a0fb
Michal8146 <michal.caha.1312@gmail.com>
2025-11-14 17:17:08 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,2 +1,19 @@
 
-print("n")
+list = [1, 4, 3, 10, -2, -3, 2.4, "a", 16, -10, 111]
+
+
+def returnOddOrEven(list, return_even):
+    new_list = []
+    for number in list:
+        if type(number) == int:
+            if return_even == True:
+                if number % 2 == 0:
+                    new_list.append(number)
+            else:
+                if number % 2 == 1:
+                    new_list.append(number)
+    return new_list
+
+print(returnOddOrEven(list, True))
+print(returnOddOrEven(list, False))
+
