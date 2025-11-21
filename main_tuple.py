76d755bcd3ed5fc906c78c9f03a338c48cbb4c60
Michal8146 <michal.caha.1312@gmail.com>
2025-11-21 16:29:05 +0100

    Update main_tuple.py

--- main_tuple.py
+++ main_tuple.py
@@ -1,10 +1,10 @@
 
-a = (2, 4, 1, 10, 7, 11, 5, -2)
+a = (2, 4, 1, 10, 7, 11, 5, -2, 4, 1, 4, 13)
 
 def find_min_max(numbers):
     min_num = numbers[0]
     max_num = numbers[0]
-    for num in numbers:
+    for num in numbers[1:]:
         if num < min_num:
             min_num = num
         elif num > max_num:
@@ -13,3 +13,13 @@ def find_min_max(numbers):
     return (min_num, max_num)
 
 print(find_min_max(a))
+
+def count_occurrences(numbers, target):
+    counter = 0
+    if target in numbers:
+        for num in numbers:
+            if num == target:
+                counter += 1
+    return counter
+
+print(count_occurrences(a, 4))
