f08e74cf1f7d59842bad613dc49c4ef3e81e5494
Michal8146 <michal.caha.1312@gmail.com>
2026-02-27 16:44:14 +0100

    commit

--- flask/main.py
+++ flask/main.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Home page</h1>"
+    return render_template('index.html')
 
 @app.route("/greet")
 def greet():
-    return "<h1>Cau!</h1>"
+    return render_template('greet.html')
 
 
 if __name__ == "__main__":
