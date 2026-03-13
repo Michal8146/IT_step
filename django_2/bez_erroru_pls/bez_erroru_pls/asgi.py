fe94345d176c380dabeb7d35465e0e8969dee755
Michal8146 <michal.caha.1312@gmail.com>
2026-03-13 17:34:41 +0100

    commit

---
+++ django_2/bez_erroru_pls/bez_erroru_pls/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for bez_erroru_pls project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bez_erroru_pls.settings')
+
+application = get_asgi_application()
---
+++ django_2/bez_erroru_pls/core/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import home, about
+
+urlpatterns = [
+    path('', home, name="home_page"),
+    path('about', about, name="about_page"),
+]
